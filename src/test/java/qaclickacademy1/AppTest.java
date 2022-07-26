d6ab26bef30fc0e619db6a1f36141f609e9b3fd1
Ashish Jhawer <ajhawer@gmail.com>
2022-07-26 18:10:24 +0530

    first commit

---
+++ src/test/java/qaclickacademy1/AppTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy1;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void NativeAPpAndroid() {
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
